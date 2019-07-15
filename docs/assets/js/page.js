ade9bf1be2cca72b9bb7c72de4d83647f324e0fb
dislido <909796264@qq.com>
2019-07-15 23:12:22 +0900

    update pages

--- docs/assets/js/page.js
+++ docs/assets/js/page.js
@@ -1,19 +1,14 @@
 {
-  const dir = [{
-    name: '首页',
-    url: '/',
-  }, {
-    name: '文档',
-    url: '/docs/',
-    children: [{
-      name: 'createRobot',
-      url: 'createrobot'
-    }],
-  }, {
-    name: '教程',
-    url: '/tutorial/',
-  }];
+  const dir = [['/', '首页'], ['/docs/', '文档'], ['/tutorial/', '教程']];
+  const mainDirectory = document.getElementById('main_directory');
+  const leftCurr = dir.findIndex(it => it[0] === pageConf.dir);
+  dir.forEach((it, index) => {
+    const p = document.createElement('p');
+    const title = document.createElement(index === leftCurr ? 'strong' : 'a');
+    if (index !== leftCurr) title.setAttribute('href', `/cqnode${it[0]}`);
+    title.innerText = it[1];
+    p.append(title);
+    mainDirectory.append(p);
+  })
 
-  const mainDirectory = document.querySelector('#main_directory ul');
-  
 }
