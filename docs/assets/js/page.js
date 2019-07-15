7c689fe6ca3280fa7a1fb42618f58539822939c4
dislido <909796264@qq.com>
2019-07-15 23:25:15 +0900

    wip pages

--- docs/assets/js/page.js
+++ docs/assets/js/page.js
@@ -1,7 +1,7 @@
 {
   const dir = [['/', '首页'], ['/docs/', '文档'], ['/tutorial/', '教程']];
   const mainDirectory = document.getElementById('main_directory');
-  const leftCurr = dir.reverse().findIndex(it => pageConf.dir.startsWith(it[0]));
+  const leftCurr = dir.length - 1 - dir.reverse().findIndex(it => pageConf.dir.startsWith(it[0]));
   dir.reverse();
   
   dir.forEach((it, index) => {
