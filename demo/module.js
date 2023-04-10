cb3cfa6f35d20132988177e9796696f473f0474c
DisLido <dislido@foxmail.com>
2023-04-10 17:17:41 +0800

    fix: demo

--- demo/module.js
+++ demo/module.js
@@ -3,50 +3,19 @@
  */
 const CQNode = require('..');
 
-class MyModule extends CQNode.Module {
-  constructor() {
-    /** (可选)添加模块信息 */
-    super({
-      name: '我的模块',
-      packageName: '@dislido/demo-mymodule',
-      help: '模块帮助信息',
-      description: '模块描述信息',
-    });
-  }
-
-  onRun() {
-    console.log(this.cqnode.inf.groupList);
-  }
-
-  /**
-   * 添加这些类型声明可以获得代码提示，如果你使用TypeScript会更加方便
-   * @param {CQNode.CQEvent.Event} data
-   * @param {CQNode.CQResponse.Message} resp
-   */
-  onMessage(data, resp) {
-    return resp.reply(`received: ${data.msg}`);
-  }
-
-  onEvent(data) {
-    if (CQNode.util.eventType.isMessage(data)) {
-      console.log(data.msg);
-    }
-  }
-
-  /**
-   * @param {CQNode.CQEvent.GroupMessage} data
-   * @param {CQNode.CQResponse.GroupMessage} resp
-   */
-  onGroupMessage(data, resp) {
-    console.log('received groupMessage');
-    return this.onMessage(data, resp);
-  }
+/** @type {CQNode.FunctionModule} */
+function MyModule(mod) {
+  mod.setMeta({
+    'packageName': 'demo-module',
+  })
+  mod.on(CQNode.CQEventType.message, console.log);
 }
 
 CQNode.createRobot({
-  modules: [new MyModule()],
+  modules: [MyModule],
   connector: {
-    LISTEN_PORT: 8080,
+    account: 191107040,
+    password: 'lido2263',
   },
 });
 
